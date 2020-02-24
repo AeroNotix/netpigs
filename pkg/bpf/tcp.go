4868659cad3a2747856315cff2e73f802dfd11c8
Aaron France <aaron.l.france@gmail.com>
2020-02-24 02:30:11 +0100

    fix: vastly reduce the eBPF program to bare minimum

--- pkg/bpf/tcp.go
+++ pkg/bpf/tcp.go
@@ -17,12 +17,8 @@ import (
 // #include <linux/sched.h>
 import "C"
 
-type ipv4_key_t struct {
-	Pid   uint32
-	Saddr uint32
-	Daddr uint32
-	Lport uint16
-	Dport uint16
+type key_t struct {
+	Pid uint32
 }
 
 type metrics struct {
@@ -50,55 +46,19 @@ struct metrics {
     u64 sent;
     u64 recv;
 };
-struct ipv4_key_t {
+struct key_t {
     u32 pid;
-    u32 saddr;
-    u32 daddr;
-    u16 lport;
-    u16 dport;
 };
-BPF_HASH(ipv4_metrics, struct ipv4_key_t, struct metrics);
-struct ipv6_key_t {
-    u32 pid;
-    // workaround until unsigned __int128 support:
-    u64 saddr0;
-    u64 saddr1;
-    u64 daddr0;
-    u64 daddr1;
-    u16 lport;
-    u16 dport;
-};
-BPF_HASH(ipv6_metrics, struct ipv6_key_t, struct metrics);
+BPF_HASH(netmetrics, struct key_t, struct metrics);
 int kprobe__tcp_sendmsg(struct pt_regs *ctx, struct sock *sk,
     struct msghdr *msg, size_t size)
 {
     u32 pid = bpf_get_current_pid_tgid();
-    u16 dport = 0, family = sk->__sk_common.skc_family;
     struct metrics *val;
-    if (family == AF_INET) {
-        struct ipv4_key_t ipv4_key = {.pid = pid};
-        ipv4_key.saddr = sk->__sk_common.skc_rcv_saddr;
-        ipv4_key.daddr = sk->__sk_common.skc_daddr;
-        ipv4_key.lport = sk->__sk_common.skc_num;
-        dport = sk->__sk_common.skc_dport;
-        ipv4_key.dport = ntohs(dport);
-        val = ipv4_metrics.lookup_or_try_init(&ipv4_key, &(struct metrics){0,0});
-        if (val) {
-            (*val).sent += size;
-        }
-    } else if (family == AF_INET6) {
-        struct ipv6_key_t ipv6_key = {.pid = pid};
-        ipv6_key.saddr0 = *(u64 *)&sk->__sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32[0];
-        ipv6_key.saddr1 = *(u64 *)&sk->__sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32[2];
-        ipv6_key.daddr0 = *(u64 *)&sk->__sk_common.skc_v6_daddr.in6_u.u6_addr32[0];
-        ipv6_key.daddr1 = *(u64 *)&sk->__sk_common.skc_v6_daddr.in6_u.u6_addr32[2];
-        ipv6_key.lport = sk->__sk_common.skc_num;
-        dport = sk->__sk_common.skc_dport;
-        ipv6_key.dport = ntohs(dport);
-        val = ipv6_metrics.lookup_or_try_init(&ipv6_key, &(struct metrics){0,0});
-        if (val) {
-            (*val).sent += size;
-        }
+    struct key_t key = {.pid = pid};
+    val = netmetrics.lookup_or_try_init(&key, &(struct metrics){0,0});
+    if (val) {
+        (*val).sent += size;
     }
     return 0;
 }
@@ -111,40 +71,35 @@ int kprobe__tcp_sendmsg(struct pt_regs *ctx, struct sock *sk,
 int kprobe__tcp_cleanup_rbuf(struct pt_regs *ctx, struct sock *sk, int copied)
 {
     u32 pid = bpf_get_current_pid_tgid();
-    u16 dport = 0, family = sk->__sk_common.skc_family;
     struct metrics *val;
     if (copied <= 0)
         return 0;
-    if (family == AF_INET) {
-        struct ipv4_key_t ipv4_key = {.pid = pid};
-        ipv4_key.saddr = sk->__sk_common.skc_rcv_saddr;
-        ipv4_key.daddr = sk->__sk_common.skc_daddr;
-        ipv4_key.lport = sk->__sk_common.skc_num;
-        dport = sk->__sk_common.skc_dport;
-        ipv4_key.dport = ntohs(dport);
-        val = ipv4_metrics.lookup_or_try_init(&ipv4_key, &(struct metrics){0,0});
-        if (val) {
-            (*val).recv += copied;
-        }
-    } else if (family == AF_INET6) {
-        struct ipv6_key_t ipv6_key = {.pid = pid};
-        ipv6_key.saddr0 = *(u64 *)&sk->__sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32[0];
-        ipv6_key.saddr1 = *(u64 *)&sk->__sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32[2];
-        ipv6_key.daddr0 = *(u64 *)&sk->__sk_common.skc_v6_daddr.in6_u.u6_addr32[0];
-        ipv6_key.daddr1 = *(u64 *)&sk->__sk_common.skc_v6_daddr.in6_u.u6_addr32[2];
-        ipv6_key.lport = sk->__sk_common.skc_num;
-        dport = sk->__sk_common.skc_dport;
-        ipv6_key.dport = ntohs(dport);
-        val = ipv6_metrics.lookup_or_try_init(&ipv6_key, &(struct metrics){0,0});
-        if (val) {
-            (*val).recv += copied;
-        }
+    struct key_t key = {.pid = pid};
+    val = netmetrics.lookup_or_try_init(&key, &(struct metrics){0,0});
+    if (val) {
+        (*val).recv += copied;
     }
+    return 0;
+}
+// todo: this isn't working right now.
+int kprobe__tcp_set_state(struct pt_regs *ctx)
+{
+    u32 pid = bpf_get_current_pid_tgid();
+    struct sock *sk = (struct sock *) PT_REGS_PARM1(ctx);
+    int state = (int) PT_REGS_PARM2(ctx);
+    if (state != TCP_CLOSE) {
+        return 0;
+    }
+    struct key_t key = {.pid = pid};
+    netmetrics.delete(&key);
     return 0;
 }
 `
 
 func pidToComm(pid uint32) string {
+	// TODO: don't need to do this - the kprobe can do it but I had
+	// issues with buffer sizes and reading garbage. Figure out what's
+	// the fix.
 	comm, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/cmdline", pid))
 	if err != nil {
 		return "<unknown>"
@@ -155,12 +110,16 @@ func pidToComm(pid uint32) string {
 
 func NewTCPTracer() {
 	m := bpf.NewModule(BPFProgram, []string{})
-	sendBytes := bpf.NewTable(m.TableId("ipv4_metrics"), m)
+	sendBytes := bpf.NewTable(m.TableId("netmetrics"), m)
 	fd0, err := m.LoadKprobe("kprobe__tcp_sendmsg")
 	if err != nil {
 		panic(err)
 	}
 	fd1, err := m.LoadKprobe("kprobe__tcp_cleanup_rbuf")
+	if err != nil {
+		panic(err)
+	}
+	fd2, err := m.LoadKprobe("kprobe__tcp_set_state")
 	if err != nil {
 		panic(err)
 	}
@@ -170,9 +129,12 @@ func NewTCPTracer() {
 	if err := m.AttachKretprobe("tcp_cleanup_rbuf", fd1, -1); err != nil {
 		panic(err)
 	}
+	if err := m.AttachKretprobe("tcp_set_state", fd2, -1); err != nil {
+		panic(err)
+	}
 	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; version=0.0.4; charset=utf-8")
-		var key ipv4_key_t
+		var key key_t
 		var value metrics
 		for it := sendBytes.Iter(); it.Next(); {
 			if err := binary.Read(bytes.NewBuffer(it.Key()), binary.LittleEndian, &key); err != nil {
