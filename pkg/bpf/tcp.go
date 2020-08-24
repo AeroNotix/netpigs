e6c5a0a8457109cec26a5d8184ce3076cfdcd25e
Aaron France <aaron.l.france@gmail.com>
2020-08-25 00:50:40 +0200

    make it fucking work

--- pkg/bpf/tcp.go
+++ pkg/bpf/tcp.go
@@ -13,114 +13,90 @@ import (
 
 import "C"
 
-type BPFMetricsKey uint32
-
-type metrics struct {
-	Send uint64
-	Recv uint64
-}
-
 const BPFProgram = `
 #include <uapi/linux/ptrace.h>
 #include <net/sock.h>
 #include <bcc/proto.h>
 #include <linux/sched.h>
 
-struct tracepoint__sock__inet__sock_set_state {
-    unsigned short common_type;
-    unsigned char common_flags;
-    unsigned char common_preempt_count;
-    int common_pid;
-    const void *skaddr;
-    int oldstate;
-    int newstate;
-    u16 sport;
-    u16 dport;
-    u8 family;
-    u8 protocol;
-    u8 saddr[4];
-    u8 daddr[4];
-    u8 saddr_v6[16];
-    u8 daddr_v6[16];
-};
+BPF_HASH(send, u32);
+BPF_HASH(recv, u32);
 
-struct metrics {
-    u64 sent;
-    u64 recv;
-};
-struct key_t {
-    u32 pid;
-};
-BPF_HASH(netmetrics, struct key_t, struct metrics);
 int kprobe__tcp_sendmsg(struct pt_regs *ctx, struct sock *sk,
     struct msghdr *msg, size_t size)
 {
-    u32 pid = (u32) (bpf_get_current_pid_tgid() >> 32);
-    struct metrics *val;
-    struct key_t key = {.pid = pid};
-    val = netmetrics.lookup_or_try_init(&key, &(struct metrics){0,0});
-    if (val) {
-        (*val).sent += size;
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    u16 dport = 0, family = sk->__sk_common.skc_family;
+    if (family == AF_INET) {
+        u32 ipv4_key = pid;
+        send.increment(ipv4_key, size);
     }
     return 0;
 }
-/*
- * tcp_recvmsg() would be obvious to trace, but is less suitable because:
- * - we'd need to trace both entry and return, to have both sock and size
- * - misses tcp_read_sock() traffic
- * we'd much prefer tracepoints once they are available.
- */
+
 int kprobe__tcp_cleanup_rbuf(struct pt_regs *ctx, struct sock *sk, int copied)
 {
-    u32 pid = (u32) (bpf_get_current_pid_tgid() >> 32);
-    struct metrics *val;
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    u16 dport = 0, family = sk->__sk_common.skc_family;
+    u64 *val, zero = 0;
     if (copied <= 0)
         return 0;
-    struct key_t key = {.pid = pid};
-    val = netmetrics.lookup_or_try_init(&key, &(struct metrics){0,0});
-    if (val) {
-        (*val).recv += copied;
+    if (family == AF_INET) {
+        u32 ipv4_key = pid;
+        recv.increment(ipv4_key, copied);
     }
-    return 0;
-}
-// todo: this isn't working right now.
-int kprobe__tcp_set_state(struct pt_regs *ctx, struct sock *sk, int state)
-{
-    u32 pid = (u32) (bpf_get_current_pid_tgid() >> 32);
-    if (state != TCP_CLOSE) {
-        return 0;
-    }
-
-    struct key_t key = {.pid = pid};
-    bpf_trace_printk("delete state: %d - %d\n", pid, netmetrics.delete(&key));
     return 0;
 }
 `
 
-func pidToComm(pid BPFMetricsKey) string {
-	// TODO: don't need to do this - the kprobe can do it but I had
-	// issues with buffer sizes and reading garbage. Figure out what's
-	// the fix.
+func pidToComm(pid uint32) (string, error) {
 	comm, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/cmdline", pid))
 	if err != nil {
-		return fmt.Sprintf("<unknown>: %d", pid)
+		return "", err
 	}
 	s := strings.Split(strings.Split(string(comm), " ")[0], "/")
-	return fmt.Sprintf("%s - %d", s[len(s)-1], pid)
+	return s[len(s)-1], nil
+}
+
+func tallyProcessThroughput(table *bpf.Table) map[string]uint64 {
+	var key uint32
+	var value uint64
+	metrics := make(map[string]uint64)
+	var toDelete [][]byte
+	for it := table.Iter(); it.Next(); {
+		if err := binary.Read(bytes.NewBuffer(it.Key()), binary.LittleEndian, &key); err != nil {
+			fmt.Println(err)
+		}
+		if err := binary.Read(bytes.NewBuffer(it.Leaf()), binary.LittleEndian, &value); err != nil {
+			fmt.Println(err)
+		}
+		if comm, err := pidToComm(key); err == nil {
+			if _, ok := metrics[comm]; ok {
+				metrics[comm] += value
+			} else {
+				metrics[comm] = value
+			}
+		} else {
+			toDelete = append(toDelete, it.Key())
+		}
+	}
+	for _, deleteKey := range toDelete {
+		if err := table.Delete(deleteKey); err != nil {
+			fmt.Println(err)
+		}
+	}
+	return metrics
 }
 
 func NewTCPTracer() {
 	m := bpf.NewModule(BPFProgram, []string{})
-	sendBytes := bpf.NewTable(m.TableId("netmetrics"), m)
+	sendBytes := bpf.NewTable(m.TableId("send"), m)
+	recvBytes := bpf.NewTable(m.TableId("recv"), m)
 	fd0, err := m.LoadKprobe("kprobe__tcp_sendmsg")
 	if err != nil {
 		panic(err)
 	}
 	fd1, err := m.LoadKprobe("kprobe__tcp_cleanup_rbuf")
-	if err != nil {
-		panic(err)
-	}
-	fd2, err := m.LoadKprobe("kprobe__tcp_set_state")
 	if err != nil {
 		panic(err)
 	}
@@ -130,22 +106,16 @@ func NewTCPTracer() {
 	if err := m.AttachKretprobe("tcp_cleanup_rbuf", fd1, -1); err != nil {
 		panic(err)
 	}
-	if err := m.AttachKretprobe("tcp_set_state", fd2, -1); err != nil {
-		panic(err)
-	}
 	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; version=0.0.4; charset=utf-8")
-		var key BPFMetricsKey
-		var value metrics
-		for it := sendBytes.Iter(); it.Next(); {
-			if err := binary.Read(bytes.NewBuffer(it.Key()), binary.LittleEndian, &key); err != nil {
-				fmt.Println(err)
-			}
-			if err := binary.Read(bytes.NewBuffer(it.Leaf()), binary.LittleEndian, &value); err != nil {
-				fmt.Println(err)
-			}
-			fmt.Fprintf(w, "bpf_network_stats_send{comm=\"%s\"} %d \n", pidToComm(key), value.Send)
-			fmt.Fprintf(w, "bpf_network_stats_recv{comm=\"%s\"} %d \n", pidToComm(key), value.Recv)
+		sendMetrics := tallyProcessThroughput(sendBytes)
+		recvMetrics := tallyProcessThroughput(recvBytes)
+
+		for comm, sendBytes := range sendMetrics {
+			fmt.Fprintf(w, "bpf_network_stats_send{comm=\"%s\"} %d \n", comm, sendBytes)
+		}
+		for comm, recvBytes := range recvMetrics {
+			fmt.Fprintf(w, "bpf_network_stats_recv{comm=\"%s\"} %d \n", comm, recvBytes)
 		}
 	})
 	panic(http.ListenAndServe(":9124", nil))
